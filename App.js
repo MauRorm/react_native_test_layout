52cd942d6cb4070d344bec6acbefb972ddfe2c2e
Your Name <you@example.com>
2019-08-19 02:09:42 -0500

    add

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { NativeRouter, Route, Link } from "react-router-native";
 import {
   StyleSheet,
   View,
@@ -10,129 +11,43 @@ import {
   ScrollView
 } from "react-native";
 
-let { height } = Dimensions.get("window");
+import Login from './containers/login/login';
 
 const CUSTOM_STATE = {
-  selectedTab: null,
-  movies: [],
 };
+
 
 export default class VerticalStackLayout extends Component {
   constructor(props) {
     super(props);
     this.state = { ...CUSTOM_STATE };
-  }
-
-  renderSection(selectedTab = null) {
-    
-    switch (selectedTab) {
-      case "1":
-        return <ScrollView>
-          {
-             <Text>uno</Text>
-          }
-        </ScrollView>;
-        break;
-      case "2":
-        return <Text>dos</Text>;
-        break;
-      case "3":
-        return <Text>Tres</Text>;
-        break;
-      default:
-        return null;
-    }
-  }
+  } 
 
   render() {
-    let box_count = 3;
-    let box_height = height / box_count;
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        flexDirection: "column"
-      },
-      box: {
-        height: box_height
-      },
-
-      //header
-      tabs: {
-        height: "15%",
-        backgroundColor: "white",
-        display: "flex",
-        flexWrap: "wrap",
-        flexDirection: "row"
-      },
-      //content
-      body: {
-        height: "80%",
-        backgroundColor: "white"
-      },
-      //footer
-      footer: {
-        height: "5%",
-        backgroundColor: "#e3aa1a"
-      },
-
-      tabOne: {
-        width: "33.33%",
-        backgroundColor: "#5DCBEE"
-      },
-      tabTwo: {
-        width: "33.33%",
-        backgroundColor: "#0EBFF7"
-      },
-      tabThree: {
-        width: "33.33%",
-        backgroundColor: "#79CBE5"
-      },
-
-      tabsText: {
-        fontSize: 40,
-        textAlign: "center"
-      }
-    });
-    const { selectedTab } = this.state;
+    const { } = this.state;
     return (
-      <View style={styles.container}>
-        <View style={styles.tabs}>
-          <TouchableOpacity
-            onPress={() => {
-              this.setState({
-                selectedTab: "1"
-              });
-            }}
-            style={styles.tabOne}
-          >
-            <Text style={styles.tabsText}>Tab1</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => {
-              this.setState({
-                selectedTab: "2"
-              });
-            }}
-            style={styles.tabTwo}
-          >
-            <Text style={styles.tabsText}>Dos</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => {
-              this.setState({
-                selectedTab: "3"
-              });
-            }}
-            style={styles.tabThree}
-          >
-            <Text style={styles.tabsText}>Tres</Text>
-          </TouchableOpacity>
-        </View>
-        <ScrollView style={styles.body}>
-          {this.renderSection(selectedTab)}
-        </ScrollView>
-        <View style={styles.footer} />
+
+      <NativeRouter>
+      <View style={styles.container}>  
+        <Route exact path="/" component={Login} />
+        {
+          /*
+                  <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
+          */
+        }
       </View>
+    </NativeRouter>
+
     );
   }
 }
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 25,
+    backgroundColor: '#3F2E71',
+  },
+});
---
+++ containers/login/login.js
@@ -0,0 +1,99 @@
+import * as React from "react";
+import { Image, StyleSheet, View, Text, TextInput } from "react-native";
+import { Input } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+import BgAudio from "react-native-background-audio";
+
+
+const strings = {
+  LOGIN: "Log In",
+  WELCOME_TO_LOGIN: "Welcome to the login screen!",
+  EMAIL_PLACEHOLDER: "Email",
+  PASSWORD_PLACEHOLDER: "Password"
+};
+
+const colors = {
+  BLACK: "#000",
+  WHITE: "#FFF",
+  DODGER_BLUE: "#428AF8",
+  SILVER: "#BEBEBE",
+  TORCH_RED: "#F8262F",
+  MISCHKA: "#E5E4E6"
+};
+
+const CUSTOM_STATE = {
+  login: "",
+  password: ""
+};
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...CUSTOM_STATE };
+  }
+
+  handleEmailChange(email) {
+    this.setState({ email: email });
+  }
+
+  handlePasswordChange(password) {
+    this.setState({ password: password });
+  }
+
+  handleLoginPress() {
+    console.log("Login button pressed");
+  }
+
+  render() {
+    const audio_options = {
+        source:{local: require('../../assets/sh-lisas-theme.mp3')}  //ex. require('./music/sample.mp3')
+      }
+    return (
+      <View style={styles.container}>
+        <BgAudio options={audio_options} />
+        <View style={{}}>
+          <Input
+            placeholder="Usuario"
+            leftIcon={<Icon name="user" size={24} color="black" />}
+            value={this.state.login}
+            editable={true}
+            onChangeText={login => this.setState({ login })}
+          />
+        </View>
+        <View style={{}}>
+          <Input
+            placeholder="Contraseña"
+            leftIcon={<Icon name="unlock-alt" size={24} color="black" />}
+            value={this.state.password}
+            editable={true}
+            onChangeText={password => this.setState({ password })}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#3F2E71",
+    width: '100%',
+    height:'100%',
+  },
+  logo: {
+    flex: 1,
+    width: "100%",
+    resizeMode: "contain",
+    alignSelf: "center"
+  },
+  form: {
+    flex: 1,
+    justifyContent: "center",
+    width: "80%"
+  }
+});
+
+export default Login;
